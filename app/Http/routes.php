7693b81c1914fe5cbc3d98884f3b58a177c4db80
francorafael <rafael.apfsantos@gmail.com>
2016-01-12 08:51:38 -0200

    Fase 3 - Rotas
    Criação das rotas
    
    Agora que você já possui os dois models criados, crie as rotas
    necessárias para que possamos realizar um CRUD em cada model.
    
    Para facilitar a administração do arquivo de rotas, as mesmas deverão
    ser totalmente agrupadas pelo prefixo: admin e pelo seu próprio model.
    
    Exemplo: admin/products, admin/categories
    
    OBS: Enquanto nem todos os controllers e actions ainda não estão
    definidos, aponte as rotas para um controller e action qualquer.
    
    OBS 2: Todas as rotas devem possuir nome e seus parâmetros devem ser
    validados.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,43 @@
 <?php
 Route::get('/', 'WelcomeController@index');
-Route::get('admin/categories', 'AdminCategoriesController@index');
-Route::get('admin/products', 'AdminProductsController@index');
+
+Route::group(['prefix'=>'admin'], function() {
+
+    //CATEGORY
+    Route::get('categories/{id?}', function($id = null) {
+        //VERIFICA SE EXISTE PARAMETRO
+        if ($id) {
+            $category = new \CodeCommerce\Category();
+            $c = $category->find($id);
+            //VERIFICA SE HÁ RESULTADOS
+            if(is_object($c))  {
+                return $c->name;
+            } else {
+                return "Sem resultados";
+            }
+        } else {
+            return "Nao ha dados para exibir";
+        }
+    });
+
+    //Produtos
+    Route::get('products/{id?}', function($id = null) {
+        //VERIFICA SE EXISTE PARAMETRO
+        if ($id) {
+            $product = new \CodeCommerce\Product();
+            $p = $product->find($id);
+            //VERIFICA SE HÁ RESULTADOS
+            if(is_object($p))  {
+                return $p->name;
+            } else {
+                return "Sem resultados";
+            }
+        } else {
+            return "Nao ha dados para exibir";
+        }
+    });
+
+});
 
 
 Route::controllers ([
